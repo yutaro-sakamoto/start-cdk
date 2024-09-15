4f630bfc0f2230947fd17a3396a8a483536acb9d
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-09-15 23:56:18 +0900

    Initial commit

---
+++ test/image-builder-4j.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ImageBuilder4J from '../lib/image-builder-4j-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/image-builder-4j-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ImageBuilder4J.ImageBuilder4JStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
