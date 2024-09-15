56b5ad78b732d2e9cd982984e3ec2fb646c8c2f0
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-09-16 00:15:09 +0900

    [Refactor]: format files using prettier

--- test/image-builder-4j.test.ts
+++ test/image-builder-4j.test.ts
@@ -4,14 +4,13 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/image-builder-4j-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new ImageBuilder4J.ImageBuilder4JStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new ImageBuilder4J.ImageBuilder4JStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
 });
