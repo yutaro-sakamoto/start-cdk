9a9e0869525dff5954fcf8e5f1767cfc72364349
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-09-16 17:00:51 +0900

    [Update]: install typedoc and add comment in lib/start-cdk.ts

--- lib/start-cdk.ts
+++ lib/start-cdk.ts
@@ -2,6 +2,9 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+/**
+ * スタック
+ */
 export class StartCDKStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
