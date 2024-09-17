5afbd124a5b4710ea12ce6b306faaf0587d4b657
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-09-17 19:49:37 +0900

    [Add]: タグとcdk-nagに関するコードを追加

--- bin/start-cdk.ts
+++ bin/start-cdk.ts
@@ -2,17 +2,14 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { StartCDKStack } from "../lib/start-cdk";
+import { AwsSolutionsChecks } from "cdk-nag";
+import { Aspects } from "aws-cdk-lib";
 
 const app = new cdk.App();
-new StartCDKStack(app, "ImageBuilder4JStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+const stack = new StartCDKStack(app, "ImageBuilder4JStack", {});
+
+// 必要に応じて作成するリソース全体に共通のタグを追加
+// cdk.Tags.of(app).add("project", "ImageBuilder4J");
+
+stack.addCdkNagSuppressions();
