a902bac14fa07d390798ceb8e8c3f0cf3b5fb025
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-11-17 21:22:41 +0900

    feat: setup a git repository

--- setup.sh
+++ setup.sh
@@ -10,3 +10,14 @@ mkdir -p $project_name
 cd $project_name
 npm init
 npm install --save-dev jest aws-cdk eslint typescript typedoc ts-jest ts-node  eslint @eslint/js @types/eslint__js typescript typescript-eslint prettier @types/jest @types/node globals
+
+git init
+git branch -m main
+
+cat <<EOF > .git/hooks/pre-commit
+#!/bin/bash
+
+npx prettier . --write
+git add -u
+EOF
+chmod +x .git/hooks/pre-commit
